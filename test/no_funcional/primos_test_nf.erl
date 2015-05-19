96cf6a0f384ce25d9fd8c98cad99652180f6cc98
Laura M. Castro <lcastro@udc.es>
2015-05-19 17:11:12 +0200

    Modificada la prueba no funcional de primos para que no falle por timeout de la prueba.

--- test/no_funcional/primos_test_nf.erl
+++ test/no_funcional/primos_test_nf.erl
@@ -1,19 +1,20 @@
 -module(primos_test_nf).
 -include_lib("eunit/include/eunit.hrl").
-
-
-
+-compile(export_all).
 
 primos_nf_test_()->
-	{MegaSecs1,Secs1,_MicroSecs1} = erlang:now(),
-	{_TASKNAME, Proc} = primos:programar(1,1000000),
-	BeginSecs = MegaSecs1 * 1000000 + Secs1,
-	Proc!{do,self()},
-	receive
+    {timeout, 5 * 60, fun caso_de_prueba/0}. % in order to avoid the test case to fail due to test timeout
+
+caso_de_prueba() ->
+    {MegaSecs1,Secs1,_MicroSecs1} = erlang:now(),
+    {_TASKNAME, Proc} = primos:programar(1,100000),
+    BeginSecs = MegaSecs1 * 1000000 + Secs1,
+    Proc!{do,self()},
+    receive
 	{State, Proc} ->
-		{MegaSecs,Secs,_MicroSecs} = erlang:now(),
-		EndSecs = MegaSecs * 1000000 + Secs,
-		?_assertEqual(State,done),
-		Dif = (EndSecs - BeginSecs),
-		?_assertEqual(Dif < 60,true)
-	end.
+	    {MegaSecs,Secs,_MicroSecs} = erlang:now(),
+	    EndSecs = MegaSecs * 1000000 + Secs,
+	    ?assertMatch(State,done),
+	    Dif = (EndSecs - BeginSecs),
+	    ?assert(Dif < 10)
+    end.
