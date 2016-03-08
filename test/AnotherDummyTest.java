38b4e2c0debd5855e2d7c59b8cff24d3cbb51dd6
Laura M. Castro <lcastro@udc.es>
2016-03-08 10:33:34 +0100

    Modificado el build.xml para que ejecute los test definidos con JUnit presentes en la carpeta test. Añadidos dos test 'dummy' de ejemplo.

---
+++ test/AnotherDummyTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AnotherDummyTest {
+
+    @Test
+    public void escenario() {
+	assertEquals("Test" + 2 + 2, "Test " + 4);
+    }
+
+}
