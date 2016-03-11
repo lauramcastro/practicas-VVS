661c58e85fee427b50b7699df94e6f5ef29b8fa7
Laura M. Castro <lcastro@udc.es>
2016-03-11 23:32:28 +0100

    Ajustados los ficheros JUnit del paquete almacén para evitar el uso de imports deprecated.

--- test/almacen/AlmacenRestringidoTest.java
+++ test/almacen/AlmacenRestringidoTest.java
@@ -10,11 +10,11 @@ import contenido.Contenido;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
-import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
