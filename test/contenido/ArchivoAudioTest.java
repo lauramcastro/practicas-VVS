4c4df9c8c0c34346949774e6962854ebfe87e583
david.torres <david.torres@udc.es>
2016-03-13 19:19:42 +0100

    Añadidos los test restantes para completar la prueba de ArchivoAudio.java

--- test/contenido/ArchivoAudioTest.java
+++ test/contenido/ArchivoAudioTest.java
@@ -44,4 +44,39 @@ public class ArchivoAudioTest {
 	assertEquals(1, resultado.size());
     }
     
+    @Test(expected = RuntimeException.class)
+	public void agregarContenidoTest(){
+		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
+		Contenido lavigne2 = new ArchivoAudio("Avril Lavigne: I can do better", "http://servidor/alavigne/bestdamnthing/2", 197, "Punk pop");
+		lavigne1.agregar(lavigne2, null);
+	}
+	@Test(expected = RuntimeException.class)
+	public void eliminarContenidoTest(){
+		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
+		lavigne1.eliminar(lavigne1);
+	}
+	
+	@Test
+	public void notEqualsTest(){
+		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
+		Contenido otro = new ArchivoAudio("Otro: Cancioncita", "http://servidor/otro/cancioncita/1", 216, "Punk pop");
+		
+		assertEquals(false,lavigne1.equals(otro));
+	}
+	
+	@Test
+	public void obtenerPadreTest(){
+		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
+		assertEquals(null,lavigne1.obtenerPadre());
+	}
+		
+	@Test
+	public void obtenerListaReproduccionTest(){
+		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
+		Collection<String> resultado = lavigne1.obtenerListaReproduccion();
+		assertEquals(1,resultado.size());
+		assertEquals(true, resultado.toArray()[0].equals("http://servidor/alavigne/bestdamnthing/1"));
+				
+	}
+    
 }
