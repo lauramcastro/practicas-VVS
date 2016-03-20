30727ad43eb79e44bf838822827ef7f179542845
Breixo Camiña Fernández <breixo2@gmail.com>
2016-03-20 06:01:45 +0100

    Se actualizaron las pruebas de ArchivoAudioTest para evitar mutaciones de código. Pendiente de revisión!

--- src/test/java/vvs/contenido/ArchivoAudioTest.java
+++ src/test/java/vvs/contenido/ArchivoAudioTest.java
@@ -1,93 +1,94 @@
 package vvs.contenido;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ArchivoAudioTest {
 
     /* Casos de que prueban la funcionalidad de busqueda sobre un contenido de tipo ArchivoAudio */
 
+	Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
+	Contenido lavigne2 = new ArchivoAudio("Avril Lavigne: I can do better", "http://servidor/alavigne/bestdamnthing/2", -197, "Punk pop");
+	Contenido eminem1 = new ArchivoAudio("Eminem", "http://servidor/alavigne/bestdamnthing/1", 216, "Rap");
+	
     @Test
     public void buscarPorVariasPalabrasTest() {
-	Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
-	Collection <Contenido> resultado = lavigne1.buscar("Avril Lavigne");
-	assertEquals(1, resultado.size());
+	    Collection <Contenido> col = new ArrayList<Contenido>();
+	    col.add(lavigne1);
+	    Collection <Contenido> resultado = lavigne1.buscar("Avril Lavigne");
+		assertEquals(resultado, col);
     }
 	
     @Test
     public void buscarPorVariasPalabrasDesordenadasTest() {
-	Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
-	Collection <Contenido> resultado = lavigne1.buscar("Lavigne Avril");
-	assertEquals(1, resultado.size());
+		Collection <Contenido> col = new ArrayList<Contenido>();
+	    col.add(lavigne1);
+		Collection <Contenido> resultado = lavigne1.buscar("Lavigne Avril");
+		assertEquals(resultado, col);
     }
     
     @Test
     public void buscarPorUnaPalabraExactaTest() {
-	Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
-	Collection <Contenido> resultado = lavigne1.buscar("Lavigne");
-	assertEquals(1, resultado.size());
+    	Collection <Contenido> col = new ArrayList<Contenido>();
+        col.add(lavigne1);
+        Collection <Contenido> resultado = lavigne1.buscar("Lavigne");
+    	assertEquals(resultado, col);
+		assertEquals(1, resultado.size());
     }
     
     @Test
     public void buscarPorUnaPalabraConMayusculasMinusculasTest() {
-	Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
-	Collection <Contenido> resultado = lavigne1.buscar("LavIgnE");
-	assertEquals(1, resultado.size());
+		Collection <Contenido> resultado = lavigne1.buscar("LavIgnE");
+		Collection <Contenido> col = new ArrayList<Contenido>();
+	    col.add(lavigne1);
+		assertEquals(resultado, col);
     }
     
     @Test
     public void buscarPorUnaPalabraConEspaciosAntesDespuesTest(){
-	Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
-	Collection <Contenido> resultado = lavigne1.buscar(" Lavigne ");
-	assertEquals(1, resultado.size());
+		Collection <Contenido> col = new ArrayList<Contenido>();
+	    col.add(lavigne1);
+	    Collection <Contenido> resultado = lavigne1.buscar(" Lavigne ");
+		assertEquals(resultado, col);
     }
     
     @Test(expected = RuntimeException.class)
 	public void agregarContenidoTest(){
-		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
-		Contenido lavigne2 = new ArchivoAudio("Avril Lavigne: I can do better", "http://servidor/alavigne/bestdamnthing/2", 197, "Punk pop");
 		lavigne1.agregar(lavigne2, null);
 	}
 	@Test(expected = RuntimeException.class)
 	public void eliminarContenidoTest(){
-		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
 		lavigne1.eliminar(lavigne1);
 	}
 	
 	@Test
 	public void notEqualsTest(){
-		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
-		Contenido otro = new ArchivoAudio("Otro: Cancioncita", "http://servidor/otro/cancioncita/1", 216, "Punk pop");
-		
-		assertEquals(false,lavigne1.equals(otro));
+		assertEquals(false, lavigne1.equals(eminem1));
 	}
 	
 	@Test
 	public void obtenerPadreTest(){
-		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
-		assertEquals(null,lavigne1.obtenerPadre());
+		assertEquals(null, lavigne1.obtenerPadre());
 	}
 		
 	@Test
 	public void obtenerListaReproduccionTest(){
-		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
 		Collection<String> resultado = lavigne1.obtenerListaReproduccion();
-		assertEquals(1,resultado.size());
 		assertEquals(true, resultado.toArray()[0].equals("http://servidor/alavigne/bestdamnthing/1"));
 				
 	}
 	
 	@Test
 	public void obtenerDuracionTest(){
-		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", 216, "Punk pop");
 		assertEquals(216, lavigne1.obtenerDuracion());
 	}
     
 	@Test
 	public void ObtenerDuracionNegativaTest(){
-		Contenido lavigne1 = new ArchivoAudio("Avril Lavigne: Girlfriend", "http://servidor/alavigne/bestdamnthing/1", -216, "Punk pop");
-		assertEquals(-216, lavigne1.obtenerDuracion());
+		assertEquals(-197, lavigne2.obtenerDuracion());
 	}
 }
