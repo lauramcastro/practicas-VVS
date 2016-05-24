02705e42d284282d1524c60e44e586d67e42ad76
adrian <adrian@10.20.38.10>
2016-05-24 19:19:31 +0200

    Adaptada la clase AlmacenRealGraphWalker a los nuevos grafos.

--- src/test/java/vvs/almacen/AlmacenRealGraphWalker.java
+++ src/test/java/vvs/almacen/AlmacenRealGraphWalker.java
@@ -1,15 +1,16 @@
 package vvs.almacen;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import vvs.contenido.Contenido;
 
 import org.junit.Assert;
 
 import org.graphwalker.core.machine.ExecutionContext;
-import org.graphwalker.java.annotation.GraphWalker;
 import vvs.contenido.ArchivoAudio;
 import vvs.contenido.ExcepcionContenido;
+import vvs.contenido.GrapAux;
 
-@GraphWalker(value = "random(edge_coverage(100))", start = "iniciar")
 public class AlmacenRealGraphWalker extends ExecutionContext implements AlmacenRealModel {
 
     private Almacen almacen;
@@ -17,28 +18,33 @@ public class AlmacenRealGraphWalker extends ExecutionContext implements AlmacenR
     
     // TRANSICIONES
     
-    public void iniciar() {
+    /*public void iniciar() {
         try {
             almacen = new AlmacenReal("Universal Music Group");
             presente = new ArchivoAudio("Amy Winehouse: Rehab", "http://servidor/winehouse/back2black/1", 215, "Soul");
         } catch (ExcepcionContenido ex) {
             System.err.println("Problema al crear contenidos");
         }
-    }
+    }*/
 
     public void agregarContenido() {
         // System.out.println("Running: agregarContenido");
 	try {
-	    almacen.agregarContenido(presente);
+            int num = GrapAux.getNumCont();
+            presente = new ArchivoAudio("Amy Winehouse: Rehab"+num, "http://servidor/winehouse/back2black/1"+num, 215, "Soul");
+	    GrapAux.getAlamcen().agregarContenido(presente);
+            GrapAux.addNumCont();
 	} catch (ExcepcionAlmacen e) {
 	    System.err.println("Problema al almacenar contenidos en almacenes");
-	}
+	} catch (ExcepcionContenido ex) {
+            Logger.getLogger(AlmacenRealGraphWalker.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void eliminarContenido() {
         // System.out.println("Running: eliminarContenido");
 	try {	    
-            almacen.eliminarContenido(presente);
+            GrapAux.getAlamcen().eliminarContenido((Contenido)GrapAux.getAlamcen().obtenerContenidos().toArray()[0]);
 	} catch (ExcepcionAlmacen e) {
 	    System.err.println("Problema al eliminar contenidos de almacenes");
 	}
@@ -46,20 +52,10 @@ public class AlmacenRealGraphWalker extends ExecutionContext implements AlmacenR
 
     // ESTADOS
     public void AlmacenVacio() {
-        // System.out.println("In state: sin proveedor");
-	try {
-	    Assert.assertTrue(almacen.buscar(presente.obtenerTitulo()).isEmpty());
-	} catch (ExcepcionAlmacen e) {
-	    System.err.println("Problema inesperado");
-	}
+        Assert.assertTrue(GrapAux.getNumCont()==0);
     }
 
     public void AlmacenConContenido() {
-        // System.out.println("In state: con proveedor");
-	try {
-	    Assert.assertFalse(almacen.buscar(presente.obtenerTitulo()).isEmpty());
-	} catch (ExcepcionAlmacen e) {
-	    System.err.println("Problema inesperado");
-	}
+        Assert.assertFalse(GrapAux.getNumCont()!=0);
     }
 }
