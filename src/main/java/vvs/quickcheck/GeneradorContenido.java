dd04660b90ab6950c3059c394bf524ab7d9419d9
fragmentados <eliasfb94@hotmail.com>
2016-05-27 17:23:06 +0200

    Cambio de clase Generador QuickCheck de Contenidos
    Se crea un nuevo paquete en el que se almacena el generador aleatorio de contenidos: GeneradorContenido.java

---
+++ src/main/java/vvs/quickcheck/GeneradorContenido.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vvs.quickcheck;
+
+import net.java.quickcheck.Generator;
+import net.java.quickcheck.generator.PrimitiveGenerators;
+import vvs.contenido.ArchivoAudio;
+import vvs.contenido.Bonus;
+import vvs.contenido.Contenido;
+import vvs.contenido.ExcepcionContenido;
+import vvs.contenido.Promocion;
+
+/**
+ *
+ * @author Elías
+ */
+public class GeneradorContenido implements Generator<Contenido> {
+
+    private Generator<String> s = PrimitiveGenerators.strings();
+    private Generator<Integer> i = PrimitiveGenerators.integers(0); // valor mínimo
+
+    @Override
+    public Contenido next() {
+        try {
+            ArchivoAudio archivo = new ArchivoAudio(s.next(), s.next(), i.next(), s.next());
+            Integer contador = i.next();
+            if (contador == 0) {
+                //Bonus
+                return new Bonus(archivo, this.next());
+            } else if (contador == 1) {
+                //Promocion
+                return new Promocion(archivo, s.next());
+            } else {
+                //Archivo Audio
+                return archivo;
+            }
+        } catch (ExcepcionContenido e) {
+            System.err
+                    .println("No se ha podido generar el contenido"
+                            + e.getMessage());
+            return null;
+        }
+    }
+}
